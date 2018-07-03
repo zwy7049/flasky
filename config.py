7363f3d9d8ab7f5ea8fb9dabb00ef822b2c4061c
zwy7049 <zwy7049@163.com>
2018-07-03 10:15:44 +0800

    change to mysql

--- config.py
+++ config.py
@@ -28,19 +28,19 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        'mysql://root:password@localhost/db'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+        'mysql://root:password@localhost/db'
     WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'mysql://root:password@localhost/db'
 
     @classmethod
     def init_app(cls, app):
